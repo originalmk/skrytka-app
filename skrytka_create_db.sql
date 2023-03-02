8828481ac3c889fea542017738fb80962374944d
Maciej Krzyżanowski <krzmaciek@gmail.com>
2023-03-02 19:23:39 +0100

    Repaired fire_truck image_path constraint

--- skrytka_create_db.sql
+++ skrytka_create_db.sql
@@ -21,13 +21,13 @@ CREATE TABLE osp_unit (
 CREATE TABLE fire_truck (
     id SERIAL PRIMARY KEY,
     name VARCHAR(64) NOT NULL,
-    image_path VARCHAR(128) CHECK(image_path ~ '^([0-9a-z_]+/?)*[0-9a-z_]+\.((png)|(jpg)|(jpeg)|(webp))$'),
+    image_path VARCHAR(128) CHECK(image_path ~ '^([0-9A-Za-z_]+/?)*[0-9A-Za-z_]+\.((png)|(jpg)|(jpeg)|(webp))$'),
     osp_unit_id INTEGER NOT NULL REFERENCES osp_unit ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE truck_side (
     id SERIAL PRIMARY KEY,
-    image_path VARCHAR(128) NOT NULL CHECK(image_path ~ '^([0-9a-z_]+/?)*[0-9a-z_]+\.((png)|(jpg)|(jpeg)|(webp))$'),
+    image_path VARCHAR(128) NOT NULL CHECK(image_path ~ '^([0-9A-Za-z_]+/?)*[0-9A-Za-z_]+\.((png)|(jpg)|(jpeg)|(webp))$'),
     ordinal_number INTEGER NOT NULL,
     fire_truck_id INTEGER REFERENCES fire_truck ON DELETE CASCADE ON UPDATE CASCADE
 );
